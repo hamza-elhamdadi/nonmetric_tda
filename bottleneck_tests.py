458ac19e0179983f10c789d34049920de5ed7122
Hamza <hme1@mail.usf.edu>
2019-09-27 16:58:59 -0400

    fixed file-writing issues

--- bottleneck_tests.py
+++ bottleneck_tests.py
@@ -1,14 +1,20 @@
 import subprocess as sp
+import os, os.path
+import glob
+import shutil
 
 sp.call(['python', 'ripser_3.py'])
 
 sp.call(['touch', 'bottleneck_values.txt'])
 
-sp.call(['rm', './persistence_diagrams/*.txt'])
-
+cmd_1 = 'echo "Metric compared to k = ' + str(1) +'" > bottleneck_values.txt'
+cmd_2 = './hera/geom_bottleneck/build/bottleneck_dist ./persistence_diagrams/persistence_diagram_metric.txt ./persistence_diagrams/persistence_diagram_nonmetric_k_' + str(1) + '.txt >> bottleneck_values.txt'
 cmd_3 = 'echo "" >> bottleneck_values.txt'
+sp.call(['sh', '-c', cmd_1])
+sp.call(['sh', '-c', cmd_2])
+sp.call(['sh', '-c', cmd_3])
 
-for i in range(10):
+for i in range(1,10):
     cmd_1 = 'echo "Metric compared to k = ' + str(i+1) +'" >> bottleneck_values.txt'
     cmd_2 = './hera/geom_bottleneck/build/bottleneck_dist ./persistence_diagrams/persistence_diagram_metric.txt ./persistence_diagrams/persistence_diagram_nonmetric_k_' + str(i+1) + '.txt >> bottleneck_values.txt'
     sp.call(['sh', '-c', cmd_1])
