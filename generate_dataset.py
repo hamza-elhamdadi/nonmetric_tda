c338ebf8644edc489c6c5793f21c13d289312f7a
Mohamed Elhamdadi <melhamdadi@mohameds-mbp-2.home>
2019-10-02 13:18:01 -0400

    Generate Datasets

--- generate_dataset.py
+++ generate_dataset.py
@@ -7,6 +7,8 @@
 import math
 import operator as op
 import numpy as np
+import matplotlib.pyplot as plt
+from itertools import chain
 import sys
 
 ###############################################################################################################################
@@ -66,6 +68,7 @@ def metric_to_nonmetric(data, mat, k):
 #                                                      Make the data                                                          #
 ###############################################################################################################################
 
+data = []
 data_set = []
 num_data_points = []
 radii = []
@@ -76,8 +79,9 @@ randomness_vals_y = []
 # Request user input for the number of circles
 
 print("Please enter the number of circles (fewer than or equal ten) you would like in your data set")
-if int(raw_input()) <= 10:
-    num_circles = int(raw_input())
+str_num_circles = raw_input()
+if int(str_num_circles) <= 10:
+    num_circles = int(str_num_circles)
 else:
     print("You entered an invalid number of circles. Aborting program.")
     sys.exit()
@@ -86,8 +90,9 @@ else:
 
 for i in range(num_circles):
     print("Please enter the number of data points (fewer than or equal to 1000) you would like in circle " + str(i+1))
-    if int(raw_input()) <= 1000:
-        num_data_points.add(int(raw_input()))
+    str_num_data_points = raw_input()
+    if int(str_num_data_points) <= 1000:
+        num_data_points.append(int(str_num_data_points))
     else:
         print("You entered an invalid number of data points. Aborting program.")
         sys.exit()
@@ -95,8 +100,9 @@ for i in range(num_circles):
 # Request user for range of randomness they would like to enter
 
 print("Please enter the range of randomness (smaller than or equal to 5) you would like to add to all of the points in the form (x y)")
-rand_range_start = raw_input()[1]
-rand_range_end = raw_input()[-1]
+rand_inputs = raw_input()
+rand_range_start = rand_inputs[0]
+rand_range_end = rand_inputs[-1]
 if int(rand_range_start) <= 5 and int(rand_range_end) <= 5 and int(rand_range_start) > 0 and int(rand_range_end) > 0:
     rand_range_start = int(rand_range_start)
     rand_range_end = int(rand_range_end)
@@ -112,8 +118,9 @@ else:
 
 for i in range(num_circles):
     print("Please enter the radius (smaller than or equal to 10) for circle" + str(i+1))
-    if int(raw_input()) <= 10:
-        radii.add(int(raw_input()))
+    curr_radius = raw_input()
+    if int(curr_radius) <= 10:
+        radii.append(int(curr_radius))
     else:
         print("You entered an invalid radius. Aborting program.")
         sys.exit()    
@@ -121,10 +128,22 @@ for i in range(num_circles):
 # Generate random values for each of the circles
 
 for i in range(num_circles):
-    randomness_vals_x.add([np.random.uniform(rand_range_start, rand_range_end) for x in range(0,num_data_points[i]+1)])
-    randomness_vals_y.add([np.random.uniform(rand_range_start, rand_range_end) for y in range(0,num_data_points[i]+1)])
+    randomness_vals_x.append([np.random.uniform(rand_range_start, rand_range_end) for x in range(0,num_data_points[i]+1)])
+    randomness_vals_y.append([np.random.uniform(rand_range_start, rand_range_end) for y in range(0,num_data_points[i]+1)])
+
+# Generate the data set
+
+center = radii[0]
 
 for i in range(num_circles):
-    if i > 0:
-        center = radii[i-1]+radii[i]
-    data_set.add([(math.cos(2*np.pi/num_data_points[i]*x)*radii[i]+center+val_x_1[x],math.sin(2*np.pi/num_data_points[i]*x)*radii[i]+randomness_vals_y[i][x]) for x in range(0, num_data_points[i]+1)])
+    if i+1 > 0:
+        center += radii[i-1]+radii[i]+1
+    data_set.append([(math.cos(2*np.pi/num_data_points[i]*x)*radii[i]+center+randomness_vals_x[i][x],math.sin(2*np.pi/num_data_points[i]*x)*radii[i]+randomness_vals_y[i][x]) for x in range(0, num_data_points[i]+1)])
+
+data = list(chain(*data_set))
+
+data_x = map(op.itemgetter(0), data)
+data_y = map(op.itemgetter(1), data)
+
+plt.scatter(data_x,data_y,np.pi*3)
+plt.show()
